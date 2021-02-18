5a93868b7c40d06feefc5ed2e04f897aaffb37d9
Abdull8870 <abrahuman8870@gmail.com>
2021-02-18 23:06:55 +0530

    Pagination Defect fixes

--- src/app/flickr-service.service.ts
+++ src/app/flickr-service.service.ts
@@ -6,96 +6,127 @@ import { PhotoArrayModel } from './Modal/photoArray.model';
 import { Photo } from './Modal/photo.model';
 import { Subject } from "rxjs";
 import { Images } from './Modal/images.model';
+import { Review } from './Modal/review.model';
 import { Router } from '@angular/router';
+import {forEach} from "@angular-devkit/schematics";
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FlickrServiceService {
-     auth:boolean=true;
-     private allImgObs=new Subject<{allImg:Images[],page:Number}>();
-     url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&`;
-     private allImages :Images[];
-
+    auth:boolean=true;
+    public pageNumber:number=1;
+    private allImgObs=new Subject<{allImg:Images[],page:Number}>();
+    url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&`;
+    private allImages :Images[];
+    private allImagesReview :Review[]=[];
     constructor(private http: HttpClient,private router: Router) { }
 
 
-  getFoodImages(currPage) {
-    const params = `api_key=${environment.key}&text=food&format=json&nojsoncallback=1&per_page=30&page=${currPage}&sort=date-posted-asc`;
-    return this.http.get(this.url + params).pipe(map((res: PhotoArrayModel) => {
-      console.log(res);
-      let urlArr:Images[] = [];
-      res.photos.photo.forEach((photo: Photo) => {
-        const photoObj = {
-          url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`,
-          title: photo.title,
-          id: photo.id,
-          rating: null,
-          givenBy: '',
-          reason: ''
-        };
-         urlArr.push(photoObj);
-      });
-      this.allImages=urlArr;
-      this.allImgObs.next({allImg:[...urlArr],page:1});
-        this.auth=false;
-      return urlArr;
-    }));
-  }
-
-
-    getImageDetails(id:string){
-       let imageArr= this.allImages.filter(x=>{
-           return x.id==id;
-       });
-       return imageArr[0];
-   }
-
-    addReview(rating: Number, givenBy: string, reason: string, id: string, pageNum: Number){
-           let index=this.allImages.findIndex(x=>{
-               console.log(x.id+'   '+id);
-               return x.id==id;
-           });
-        this.allImages[index].rating = rating;
-        this.allImages[index].givenBy = givenBy;
-        this.allImages[index].reason = reason;
-        this.postImageDetailsInDB();
-        setTimeout(() => {
-            this.allImgObs.next({allImg:[...this.allImages], page: Number(pageNum)});
-        }, 1000);
+    getFoodImages(currPage) {
+        const params = `api_key=${environment.key}&text=food&format=json&nojsoncallback=1&per_page=30&page=${currPage}&sort=date-posted-asc`;
+        return this.http.get(this.url + params).pipe(map((res: PhotoArrayModel) => {
+            let urlArr:Images[] = [];
+            res.photos.photo.forEach((photo: Photo) => {
+                const photoObj = {
+                    url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`,
+                    title: photo.title,
+                    id: photo.id,
+                    rating: null,
+                    givenBy: '',
+                    reason: '',
+                    review:false
+                };
+                urlArr.push(photoObj);
+            });
+            this.allImages=urlArr;
+            this.allImgObs.next({allImg:[...urlArr],page:1});
+            this.auth=false;
+            return this.checkReview(this.allImages);
+        }));
     }
 
 
-   getAllImageAsObservable(){
+    getImageDetails(id:string){
+        let imageArr= this.allImages.filter(x=>{
+            return x.id==id;
+        });
+        return imageArr[0];
+    }
+
+    addReview(rating: Number, givenBy: string, reason: string, id: string, pageNum: Number){
+
+        let review = {
+            id: id,
+            rating: rating,
+            givenBy: givenBy,
+            reason: reason
+        };
+        this.allImagesReview.push(review);
+    }
+
+
+    getAllImageAsObservable(){
         console.log('inside');
         return this.allImgObs.asObservable();
-   }
+    }
 
-   postImageDetailsInDB() {
+    postImageDetailsInDB() {
         this.http.delete('https://backend-3616e.firebaseio.com/post/-MTiHhAl-ju5hqUjPVZw.json').subscribe((res:{name:string})=>{
 
         },err=>{
             console.log(err);
         });
-   }
+    }
 
-   getImages(){
+    getImages(){
         this.http.get('https://backend-3616e.firebaseio.com/post/-MTiHhAl-ju5hqUjPVZw.json')
             .subscribe(res=>{
                 console.log(res);
             },err=>{
                 console.log(err);
             });
-   }
+    }
 
-   updateObs(){
-       setTimeout(() => {
-           this.allImgObs.next({allImg:[...this.allImages],page:1});
-       }, 1000);
-   }
+    updateObs(){
+        setTimeout(() => {
+            this.allImgObs.next({allImg:[...this.allImages],page:1});
+        }, 1000);
+    }
 
-   canAllow(){
+    canAllow(){
         return this.auth;
-   }
+    }
+
+    checkReview(allImages:Images[]) {
+        let isReviewPresent:boolean=false;
+        let allImgArray:Images[]=[];
+        if( this.allImagesReview.length > 0) {
+            allImages.forEach((x) => {
+                this.allImagesReview.forEach((y) => {
+                    if(x.id===y.id) {
+                        x.rating=y.rating;
+                        x.givenBy=y.givenBy;
+                        x.reason=y.reason;
+                        x.review=true
+                        allImgArray.push(x);
+                        isReviewPresent=true;
+                    }else {
+                        isReviewPresent=false;
+                    }
+                });
+                if(!isReviewPresent && !x.review) {
+                    allImgArray.push(x);
+                }
+            });
+            return allImgArray;
+        }
+        else {
+            return allImages;
+        }
+    }
+
+
+
 }
--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,81 +1,123 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlickrServiceService } from '../flickr-service.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-food',
-  templateUrl: './food.component.html',
-  styleUrls: ['./food.component.scss']
+    selector: 'app-food',
+    templateUrl: './food.component.html',
+    styleUrls: ['./food.component.scss']
 })
-export class FoodComponent implements OnInit ,OnDestroy{
-   loading:boolean;
-   pages=[1,2,3,4];
-   images = [];
-   currentPage: Number = 1;
-   allImages=[];
-   private imgSub:Subscription;
+export class FoodComponent implements OnInit {
+    loading:boolean;
+    pages=[1,2,3,4,5,6,7,8,9,10];
+    images = [];
+    currentPage: number = 1;
+    allImages=[];
+    navigation:string;
+    private imgSub:Subscription;
 
-  constructor(private flickrService: FlickrServiceService,private router: Router) {}
+    constructor(private flickrService: FlickrServiceService,private router: Router) {}
 
-  ngOnInit(): void {
-      this.loading=true;
-   console.log("onInit");
-   this.imgSub = this.flickrService.getAllImageAsObservable().subscribe(res => {
-       console.log(res);
-       this.allImages=res.allImg;
-       let page:Number=res.page;
-       this.onChangePage(page);
-    });
-  }
-
-
-  onClick(id: string, title: string, url: string){
-    console.log(id +' '+ title +' '+ url);
-    let obj={
-      id:id,
-      title:title,
-      url:url
-    };
-    this.router.navigate(['/review'],  { queryParams: { id: id ,page:this.currentPage} });
-  }
-
-
-  onChangePage(i: Number) {
-
-      this.currentPage = i;
-      let temp= this.allImages;
-      console.log(temp);
-
-    switch (i) {
-        case 1:
-            this.images=temp.slice(0,9);
-            console.log(i);
-            console.log(this.images);
+    ngOnInit(): void {
+        this.loading=true;
+        this.currentPage=this.flickrService.pageNumber;
+        this.initialPagination(this.currentPage);
+        console.log(this.currentPage);
+        this.flickrService.getFoodImages(this.currentPage).toPromise().then(res => {
+            this.images = res;
             this.loading=false;
-            break;
-        case 2:
-            this.images=temp.slice(9,18);
-            this.loading=false;
-            break;
-        case 3:
-            this.images=temp.slice(18,27);
-            this.loading=false;
-            break;
-        case 4:
-            this.images=temp.slice(27,30);
-            this.loading=false;
-            break;
-        default:
-            this.loading=false;
-            break;
+        });
+
     }
 
-  }
+
+    onClick(id: string, title: string, url: string){
+        console.log(id +' '+ title +' '+ url);
+        let obj={
+            id:id,
+            title:title,
+            url:url
+        };
+        this.router.navigate(['/review'],  { queryParams: { id: id ,page:this.currentPage} });
+    }
 
 
-  ngOnDestroy(){
-      console.log('onDestroy');
-    this.imgSub.unsubscribe();
-  }
+    onChangePage(i: any) {
+
+        this.loading=true;
+        let tempPage:number=this.currentPage;
+        let tempNav:string=this.navigation;
+        if(isNaN(i)){
+            if(i=='N') {
+                this.currentPage = this.currentPage + 1;
+                this.flickrService.pageNumber=this.currentPage;
+                this.navigation='N';
+            } else if(i=='P'){
+                this.currentPage = this.currentPage - 1;
+                this.flickrService.pageNumber=this.currentPage;
+                this.navigation='P';
+            }
+        }else {
+            this.currentPage = i;
+            this.flickrService.pageNumber=this.currentPage;
+            this.navigation= i > tempPage ? 'N' : 'P';
+        }
+        console.log(this.currentPage,tempPage);
+        this.changePaination(this.currentPage,tempPage,tempNav);
+    }
+
+    changePaination(currPage:number,i:number,tempNav:string) {
+        if( currPage % 10 == 0 && currPage > i ) {
+            this.loading=true;
+            this.pages=[];
+            for (let i = currPage; i<=currPage+10 ; i++) {
+                this.pages.push(i);
+            }
+            this.loading=false;
+        } else if(currPage % 10 == 0 && currPage < i){
+            this.loading=true;
+            this.pages=[];
+            for (let i = currPage-10; i<=currPage ; i++) {
+                if(i>0){
+                    this.pages.push(i);
+                }
+            }
+            this.loading=false;
+        } else if (i % 10 == 0 && currPage > i && tempNav === 'P'){
+            this.loading=true;
+            this.pages=[];
+            for (let i = currPage-1; i<currPage+10 ; i++) {
+                this.pages.push(i);
+            }
+            this.loading=false;
+        } else if (i % 10 == 0 && currPage < i && tempNav === 'N') {
+            this.loading=true;
+            this.pages=[];
+            for (let i = currPage-9 ; i <= currPage+1 ; i++) {
+                if(i>0){
+                    this.pages.push(i);
+                }
+            }
+            this.loading=false;
+        }
+        this.flickrService.getFoodImages(currPage).toPromise().then(res => {
+            this.images = res;
+            this.loading=false;
+        });
+    }
+
+    initialPagination(i:number){
+        while(i%10!=0){
+            i++;
+        }
+        if(i>10) {
+            this.pages=[];
+            for (let j = i - 10; j <= i; j++) {
+                this.pages.push(j);
+            }
+        }
+    }
+
+
 }
